02eb3e093ce7918f1adaeb4327f9cbe8cdc4bfb1
shirishdixit <shirishdixit2218@gmail.com>
2025-05-24 14:25:52 +0530

    Add files via upload

---
+++ ExpenseType.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class ExpenseType {
+    
+    public static void main(String[] args) {
+        
+        String Dinner = "1000";
+        String Breakfast = "2000";
+        String CarRental = "3000";
+        int sum = 0;
+        {
+        
+          System.out.println("Meal Expenses Type");
+          
+          System.out.println("Dinner + Breakfast + CarRental All Adition");
+          
+          sum = 1000 + 2000 + 3000;
+          
+          System.out.println("Addition" +sum);
+          
+          
+        }
+          
+        
+    }
+}
